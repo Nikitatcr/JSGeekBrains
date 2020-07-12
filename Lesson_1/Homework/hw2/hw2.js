bb78a4d9adaece0d06b2b5aeccc971f5c733a781
Nikita TCR <nikitatcr@gmail.com>
2020-07-12 22:02:05 +0300

    Сделал первое Д/З по JavaScript. Тестирую GitHub

---
+++ Lesson_1/Homework/hw2/hw2.js
@@ -0,0 +1,4 @@
+'use strict';
+let admin = prompt("Введите ваше имя: ");
+let name = admin;
+alert(`Ваше имя: ${admin}!`);
---
+++ Lesson_1/Homework/hw4/hw4.js
@@ -0,0 +1,7 @@
+"use strict";
+
+/*let __hello__ = "world";
+alert(__hello__);
+
+let $$$ = "100$";
+alert($$$);*/
